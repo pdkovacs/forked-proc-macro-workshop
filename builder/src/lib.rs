47981ad0b7852b9976171b89b8638e9f91506d4c
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2022-04-18 16:34:13 +0200

    builder: step01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,13 @@
 use proc_macro::TokenStream;
 
+trait Builder {
+    type BuilderType;
+
+    fn builder() -> Self::BuilderType; 
+}
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
-
-    unimplemented!()
+    return TokenStream::new();
 }
