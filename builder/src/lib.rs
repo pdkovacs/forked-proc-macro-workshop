5c67adb2982ef6eb95725b62a2e745659158a485
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2022-05-02 08:50:41 +0200

    builder: factor out "quote_by_field_definitions"

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -3,7 +3,7 @@ use proc_macro2::{Span};
 use syn::{DeriveInput,parse_macro_input,Ident,Data,Fields, Type, TypePath, Path, PathArguments, AngleBracketedGenericArguments, GenericArgument};
 use quote::*;
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive_builder(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -12,10 +12,10 @@ pub fn derive_builder(input: TokenStream) -> TokenStream {
 
     let builder_ident = Ident::new(&format!("{}Builder", to_be_built_type), Span::call_site());
 
-    let builder_field_definitions = create_builder_field_definitions(&input.data);
-    let initial_builder_fields = create_initial_builder_fields(&input.data);
-    let builder_setter_methods = create_builder_setter_methods(&input.data);
-    let build_assignments = create_build_assignments(&input.data);
+    let builder_field_definitions = quote_by_field_definitions(&input.data, create_builder_field_definitions);
+    let initial_builder_fields = quote_by_field_definitions(&input.data, create_initial_builder_fields);
+    let builder_setter_methods = quote_by_field_definitions(&input.data, create_builder_setter_methods);
+    let build_assignments = quote_by_field_definitions(&input.data, create_build_assignments);
 
     let expanded = quote! {
         use std::error::Error;
@@ -51,27 +51,14 @@ pub fn derive_builder(input: TokenStream) -> TokenStream {
     return TokenStream::from(expanded);
 }
 
-fn create_builder_field_definitions(data: &Data) -> proc_macro2::TokenStream {
+type FieldsIterator <'a> = syn::punctuated::Iter<'a, syn::Field>;
+
+fn quote_by_field_definitions(data: &Data, fields_processor: fn(FieldsIterator) -> proc_macro2::TokenStream)  -> proc_macro2::TokenStream {
     match *data {
         Data::Struct(ref data) => {
             match data.fields {
                 Fields::Named(ref fields) => {
-                    let recurse = fields.named.iter().map(|f| {
-                        let name = &f.ident;
-                        let ty = &f.ty;
-                        if let Some(_) = unwrap_optional(ty) {
-                            quote! {
-                                #name: #ty
-                            }
-                        } else {
-                            quote! {
-                                #name: Option<#ty>
-                            }
-                        }
-                    });
-                    quote! {
-                        #(#recurse),*
-                    }
+                    fields_processor(fields.named.iter())
                 }
                 Fields::Unnamed(_) | Fields::Unit => unimplemented!()
             }
@@ -80,93 +67,82 @@ fn create_builder_field_definitions(data: &Data) -> proc_macro2::TokenStream {
     }
 }
 
-fn create_initial_builder_fields(data: &Data) -> proc_macro2::TokenStream {
-    match *data {
-        Data::Struct(ref data) => {
-            match data.fields {
-                Fields::Named(ref fields) => {
-                    let recurse = fields.named.iter().map(|f| {
-                        let field_ident = &f.ident;
-                        quote! {
-                            #field_ident: None
-                        }
-                    });
-                    quote! {
-                        #(#recurse),*
-                    }
-                }
-                Fields::Unnamed(_) | Fields::Unit => unimplemented!()
+fn create_builder_field_definitions(fields_iterator: FieldsIterator) -> proc_macro2::TokenStream {
+    let recurse = fields_iterator.map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if let Some(_) = unwrap_optional(ty) {
+            quote! {
+                #name: #ty
+            }
+        } else {
+            quote! {
+                #name: Option<#ty>
             }
         }
-        Data::Enum(_) | Data::Union(_) => unimplemented!()
+    });
+    quote! {
+        #(#recurse),*
     }
 }
 
-fn create_builder_setter_methods(data: &Data) -> proc_macro2::TokenStream {
-    match *data {
-        Data::Struct(ref data) => {
-            match data.fields {
-                Fields::Named(ref fields) => {
-                    let recurse = fields.named.iter().map(|f| {
-                        let field_ident = &f.ident;
-                        let field_type = &f.ty;
-                        let opt_unwrapped_optional = unwrap_optional(field_type);
-                        if let Some(unwrapped_optional) = opt_unwrapped_optional {
-                            quote! {
-                                fn #field_ident(&mut self, #field_ident: #unwrapped_optional) -> &mut Self {
-                                    self.#field_ident = Some(#field_ident);
-                                    self
-                                }
-                            }
-                        } else {
-                            quote! {
-                                fn #field_ident(&mut self, #field_ident: #field_type) -> &mut Self {
-                                    self.#field_ident = Some(#field_ident);
-                                    self
-                                }
-                            }
-                        }
-                    });
-                    quote! {
-                        #(#recurse)*
-                    }
-                }
-                Fields::Unnamed(_) | Fields::Unit => unimplemented!()
-            }
+fn create_initial_builder_fields(fields_iterator: FieldsIterator) -> proc_macro2::TokenStream {
+    let recurse = fields_iterator.map(|f| {
+        let field_ident = &f.ident;
+        quote! {
+            #field_ident: None
         }
-        Data::Enum(_) | Data::Union(_) => unimplemented!()
+    });
+    quote! {
+        #(#recurse),*
     }
 }
 
-fn create_build_assignments(data: &Data) -> proc_macro2::TokenStream {
-    match *data {
-        Data::Struct(ref data) => {
-            match data.fields {
-                Fields::Named(ref fields) => {
-                    let recurse = fields.named.iter().map(|f| {
-                        let field_ident = &f.ident;
-                        let field_name = stringify!(field_ident);
-                        if let Some(_) = unwrap_optional(&f.ty) {
-                            quote! {
-                                #field_ident: match self.#field_ident {
-                                    Some(_) => self.#field_ident.take(),
-                                    None => None
-                                }
-                            }
-                        } else {
-                            quote! {
-                                #field_ident: self.#field_ident.take().ok_or_else(|| Box::<dyn Error>::from(format!("{} isn't set", #field_name)))?,
-                            }    
-                        }
-                    });
-                    quote! {
-                        #(#recurse)*
-                    }
+fn create_builder_setter_methods(fields_iterator: FieldsIterator) -> proc_macro2::TokenStream {
+    let recurse = fields_iterator.map(|f| {
+        let field_ident = &f.ident;
+        let field_type = &f.ty;
+        let opt_unwrapped_optional = unwrap_optional(field_type);
+        if let Some(unwrapped_optional) = opt_unwrapped_optional {
+            quote! {
+                fn #field_ident(&mut self, #field_ident: #unwrapped_optional) -> &mut Self {
+                    self.#field_ident = Some(#field_ident);
+                    self
                 }
-                Fields::Unnamed(_) | Fields::Unit => unimplemented!()
+            }
+        } else {
+            quote! {
+                fn #field_ident(&mut self, #field_ident: #field_type) -> &mut Self {
+                    self.#field_ident = Some(#field_ident);
+                    self
+                }
             }
         }
-        Data::Enum(_) | Data::Union(_) => unimplemented!()
+    });
+    quote! {
+        #(#recurse)*
+    }
+}
+
+fn create_build_assignments(fields_iterator: FieldsIterator) -> proc_macro2::TokenStream {
+    let recurse = fields_iterator.map(|f| {
+        let field_ident = &f.ident;
+        let field_name = stringify!(field_ident);
+        if let Some(_) = unwrap_optional(&f.ty) {
+            quote! {
+                #field_ident: match self.#field_ident {
+                    Some(_) => self.#field_ident.take(),
+                    None => None
+                }
+            }
+        } else {
+            quote! {
+                #field_ident: self.#field_ident.take().ok_or_else(|| Box::<dyn Error>::from(format!("{} isn't set", #field_name)))?,
+            }    
+        }
+    });
+    quote! {
+        #(#recurse)*
     }
 }
 
@@ -178,9 +154,8 @@ fn unwrap_optional(ty: &Type) -> Option<&Type> {
             segments: path_segments
         }
     }) = ty {
-        let option_name = String::from("Option");
         for item in path_segments {
-            if item.ident.to_string() == option_name {
+            if item.ident.to_string() == String::from("Option") {
                 if let PathArguments::AngleBracketed(
                     AngleBracketedGenericArguments {
                         colon2_token: _,
